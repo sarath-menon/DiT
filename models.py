f72413ead51615943149bfc70af2b6fe4cc7a7f2
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-22 11:46:34 +0200

    cleanup

--- models.py
+++ models.py
@@ -119,29 +119,25 @@ class DiTBlock(nn.Module):
         approx_gelu = lambda: nn.GELU(approximate="tanh")
         self.mlp = Mlp(in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu, drop=0)
 
-        # AdaLN modulation: to add conditional information to the input
-        self.adaLN_modulation = nn.Sequential(
-            nn.SiLU(),
-            nn.Linear(hidden_size, 6 * hidden_size, bias=True)
-        )
+        self.adaLN_out = 6
+        self.adaLN_modulation = AdaLNModulation(hidden_size, self.adaLN_out)
 
     # Attention + MLP with residual connection
     def forward(self, x, c):
         
         # modulate -> scale and shift layer norm output 
         # gate_msa -> scale the attention output
-        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(c).chunk(6, dim=1)
+        shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = self.adaLN_modulation(c).chunk(self.adaLN_out , dim=1)
         
         x = x + gate_msa.unsqueeze(1) * self.attn(modulate(self.norm1(x), shift_msa, scale_msa))
-
         x = x + gate_mlp.unsqueeze(1) * self.mlp(modulate(self.norm2(x), shift_mlp, scale_mlp))
         return x
 
 class AdaLNModulation(nn.Module):
-    def __init__(self, hidden_size):
+    def __init__(self, hidden_size, out_scale):
         super().__init__()
         self.silu = nn.SiLU()
-        self.linear = nn.Linear(hidden_size, 2 * hidden_size, bias=True)
+        self.linear = nn.Linear(hidden_size, out_scale * hidden_size, bias=True)
 
     def forward(self, x):
         x = self.silu(x)
@@ -157,15 +153,12 @@ class FinalLayer(nn.Module):
         super().__init__()
         self.norm_final = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
         self.linear = nn.Linear(hidden_size, patch_size * patch_size * out_channels, bias=True)
-        self.adaLN_modulation = AdaLNModulation(hidden_size)
 
-        # self.adaLN_modulation = nn.Sequential(
-        #     nn.SiLU(),
-        #     nn.Linear(hidden_size, 2 * hidden_size, bias=True)
-        # )
+        self.adaLN_out =2
+        self.adaLN_modulation = AdaLNModulation(hidden_size, out_scale=2)
 
     def forward(self, x, c):
-        shift, scale = self.adaLN_modulation(c).chunk(2, dim=1)
+        shift, scale = self.adaLN_modulation(c).chunk(self.adaLN_out, dim=1)
         x = modulate(self.norm_final(x), shift, scale)
         x = self.linear(x)
         return x
@@ -243,8 +236,8 @@ class DiT(nn.Module):
 
         # Zero-out adaLN modulation layers in DiT blocks:
         for block in self.blocks:
-            nn.init.constant_(block.adaLN_modulation[-1].weight, 0)
-            nn.init.constant_(block.adaLN_modulation[-1].bias, 0)
+            nn.init.constant_(block.adaLN_modulation.linear.weight, 0)
+            nn.init.constant_(block.adaLN_modulation.linear.bias, 0)
 
         # Zero-out output layers:
         nn.init.constant_(self.final_layer.adaLN_modulation.linear.weight, 0)
