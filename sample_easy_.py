2f732c2cbd381312cdfb7f5ec515382ce7fe8c8e
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-24 11:07:35 +0200

    added func to space timesteps

--- sample_easy_.py
+++ sample_easy_.py
@@ -90,12 +90,38 @@ def p_sample_loop(model_output, x, t, gd):
 
     return x_prev 
 
+def space_timesteps(num_timesteps, section_counts):
+    size_per = num_timesteps // len(section_counts)
+    extra = num_timesteps % len(section_counts)
+    start_idx = 0
+    all_steps = []
+    for i, section_count in enumerate(section_counts):
+        size = size_per + (1 if i < extra else 0)
+        if size < section_count:
+            raise ValueError(
+                f"cannot divide section of {size} steps into {section_count}"
+            )
+        if section_count <= 1:
+            frac_stride = 1
+        else:
+            frac_stride = (size - 1) / (section_count - 1)
+        cur_idx = 0.0
+        taken_steps = []
+        for _ in range(section_count):
+            taken_steps.append(start_idx + round(cur_idx))
+            cur_idx += frac_stride
+        all_steps += taken_steps
+        start_idx += size
+    return list(all_steps)
+
+
 def inference(x,y):
 
     # create params for gaussian diffusion
     indices = list(range(n_sampling_steps))[::-1]
     indices = tqdm(indices) # for progres bar
-    map_ts = th.tensor([  0, 250, 500, 749, 999])
+    # map_ts = th.tensor([  0, 250, 500, 749, 999])
+    map_ts = space_timesteps(diffusion_steps, [n_sampling_steps])
 
     betas = linear_beta_schedule(diffusion_steps)
     betas, _ = respace_betas(betas, map_ts)
